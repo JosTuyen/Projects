39bc31b93f63c2ead19fba0cb787d400b5a32c51
JosTuyen <doductuyence@gmail.com>
2018-03-24 05:17:24 -0500

    First Commit

--- Wordnik Lookup Extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(reciever);
-
-var word = " ";
-
-function reciever(request, sender, sendRespond) {
-  console.log(request);
-  word = request.text;
-}
--- Wordnik Lookup Extension/content.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('mouseup', wordSelected);
-
-function wordSelected() {
-  var words = window.getSelection().toString();
-  if (words.length > 0) {
-    var message = {text : words};
-    chrome.runtime.sendMessage(message);
-  }
-}
